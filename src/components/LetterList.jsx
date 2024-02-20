a4884498fcb9050d46808abec1fed3fa8ec1e13b
rlaeodus <gwg03185@gmail.com>
2024-02-20 10:28:11 +0900

    Add : api.js - 인스턴스 설정 Feat : 데이터 최신순 정렬

--- src/components/LetterList.jsx
+++ src/components/LetterList.jsx
@@ -12,16 +12,15 @@ export default function LetterList() {
   const { isLoading, error, letters } = useSelector((state) => state.letters);
   // const letters = useSelector((state) => state.letters.letters);
 
+  console.log("letters", letters);
   const activeMember = useSelector((state) => state.member);
 
   // console.log("activeMember", activeMember);
-
-  console.log("letters", letters);
-
+  const aa = useSelector((state) => state);
+  console.log("aa", aa);
   const filteredLetters = letters.filter(
     (letter) => letter.writedTo === activeMember
   );
-
   if (isLoading) {
     return <div>로딩중</div>;
   }
--- src/redux/modules/lettersSlice.js
+++ src/redux/modules/lettersSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import api from "../../axios/api";
 
 const initialState = {
   letters: [],
@@ -15,17 +15,18 @@ export const letterSlice = createSlice({
   extraReducers: (builder) => {
     builder
       //__getLetters
+      //스토어 접근가능
       .addCase(__getLetters.pending, (state, action) => {
         state.isLoading = true;
         state.isError = false;
       })
       .addCase(__getLetters.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.isError = true;
+        state.isError = false;
         state.letters = action.payload;
       })
       .addCase(__getLetters.rejected, (state, action) => {
-        state.isLoading = true;
+        state.isLoading = false;
         state.isError = true;
         state.error = action.payload;
       })
@@ -37,22 +38,26 @@ export const letterSlice = createSlice({
       })
       .addCase(__addLetters.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.isError = true;
+        state.isError = false;
         state.letters = [action.payload, ...state.letters];
       })
       .addCase(__addLetters.rejected, (state, action) => {
-        state.isLoading = true;
+        state.isLoading = false;
         state.isError = true;
         state.error = action.payload;
       });
   },
 });
 
+// 전에 알던 스토어에 저장하는 로직은 return쪽
 export const __getLetters = createAsyncThunk(
   "letters/getLetters",
   async (payload, thunkAPI) => {
     try {
-      const response = await axios.get("http://localhost:5000/letters");
+      // -createdAt 내림차순 정렬
+      const response = await api.get("/letters?_sort=-createdAt");
+
+      console.log("response", response);
       // const response = await axios.get("http://localhost:5000/letters", {
       //   method: "GET",
       //   headers: {
@@ -60,14 +65,17 @@ export const __getLetters = createAsyncThunk(
       //     Authorization: `Bearer ${accessToken}`,
       //   },
       // });
-      console.log("response", response);
+      console.log("response.data", response.data);
       // const data = await response.json();
       // console.log("data", data);
       // console.log("2");
+
+      // dispatch를 해주는 기능을 가진 api
       return thunkAPI.fulfillWithValue(response.data);
     } catch (error) {
       alert("에러발생");
       console.log(error);
+      // dispatch를 해주는 기능을 가진 api
       return thunkAPI.rejectWithValue(error);
     }
   }
@@ -77,16 +85,15 @@ export const __addLetters = createAsyncThunk(
   "letters/addLetters",
   async (payload, thunkAPI) => {
     try {
-      const response = await axios.post(
-        "http://localhost:5000/letters",
-        payload
-      );
+      const response = await api.post("/letters", payload);
       console.log("payload", payload);
-      console.log("response", response);
+      console.log("response.data", response.data);
+      // dispatch를 해주는 기능을 가진 api
       return thunkAPI.fulfillWithValue(response.data);
     } catch (error) {
       alert("에러발생!");
       console.log(error);
+      // dispatch를 해주는 기능을 가진 api
       return thunkAPI.rejectWithValue(error);
     }
   }
